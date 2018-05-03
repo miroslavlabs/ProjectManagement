b18dcca41d4248b8189c4775c9df847178818eab
Miroslav Shtarbev <miroslav.shtarbev@gmail.com>
2018-05-03 07:42:45 +0300

    Began adding changes for the creation of states.

--- server-app/src/app/routes/BoardRouterProvider.ts
+++ server-app/src/app/routes/BoardRouterProvider.ts
@@ -22,8 +22,8 @@ export class BoardRouterProvider implements EntityRouterProvider {
         let router = Router();
         
         let boardEndpoint = config.routes.board;
-        router.get(`${boardEndpoint}/project/:id`, this.getAllBoards());
-        router.post(`${boardEndpoint}/project/:id`, this.createBoard());
+        router.get(`${boardEndpoint}`, this.getAllBoards());
+        router.post(`${boardEndpoint}`, this.createBoard());
         router.put(`${boardEndpoint}/:id`, this.updateBoard());
         router.get(`${boardEndpoint}/:id`, this.getBoard());
 
@@ -33,7 +33,7 @@ export class BoardRouterProvider implements EntityRouterProvider {
     private getAllBoards() {
         return (req: Request, res: Response, next: NextFunction) => {
             this.neo4jBoardsDataProvider.getAllBoards(
-                +req.params["id"],
+                +req.query["projectId"],
                 (boards: Board[]) => {
                     res.send(boards);
                     next();
@@ -63,7 +63,7 @@ export class BoardRouterProvider implements EntityRouterProvider {
     private createBoard(): (req: Request, res: Response, next: NextFunction) => void {
         return (req: Request, res: Response, next: NextFunction) => {
             this.neo4jBoardsDataProvider.createBoard(
-                +req.params["id"] /*project ID*/,
+                +req.query["projectId"],
                 req.body /*board JSON data*/,
                 (projects: Board[]) => {
                     res.send(projects[0]);
